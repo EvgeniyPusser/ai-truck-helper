e1434995852e95c30bed7cd615f36fe24b3cf301
Evgenii Bogomazov-Pusser <evgeniipusser88@gmail.com>
2025-09-01 23:37:58 +0300

    killed comments

--- frontend/src/components/USAMap.jsx
+++ frontend/src/components/USAMap.jsx
@@ -1,34 +1,4 @@
 
-// import { MapContainer, TileLayer } from 'react-leaflet';
-// import 'leaflet/dist/leaflet.css';
-
-// const USA_BOUNDS = [
-//   [24.396308, -125.0],
-//   [49.384358, -66.93457]
-// ];
-
-// export default function USAMap() {
-//   return (
-//     <MapContainer
-//       center={[37.8, -96]}
-//       zoom={4}
-//       style={{ width: '100%', height: '400px', borderRadius: '12px', boxShadow: '0 2px 8px rgba(0,0,0,0.1)' }}
-//       maxBounds={USA_BOUNDS}
-//       maxBoundsViscosity={1.0}
-//       scrollWheelZoom={true}
-//       minZoom={3}
-//       maxZoom={10}
-//       attributionControl={false}
-//     >
-//       <TileLayer
-//         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-//       />
-//     </MapContainer>
-//   );
-// }
-
-// ...existing code...
-// @ts-nocheck.
 import { useEffect, useState } from "react";
 import { MapContainer, TileLayer, Polyline, useMap } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
