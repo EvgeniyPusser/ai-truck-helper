c68267e69348fb92c1786044b92dd0f32962dae2
EvgeniyPusser <evggeniipusser88@gmail.com>
2026-04-09 19:56:33 +0300

    nothing new

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,6 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 
 import LandingPage from "./pages/LandingPage";
-import HelperPage from "./pages/HelperPage";
-import CompanyPage from "./pages/CompanyPage";
-// import TruckPage from "./pages/TruckPage";
-// import DriverPage from "./pages/DriverPage";
-import AgentPage from "./pages/AgentPage";
 import ResultPage from "./pages/ResultPage";
 
 function App() {
@@ -14,12 +9,7 @@ function App() {
     <Routes>
       <Route path="/" element={<LandingPage />} />
   <Route path="/result" element={<ResultPage />} />
-      <Route path="/helper" element={<HelperPage />} />
-      <Route path="/company" element={<CompanyPage />} />
-      {/* <Route path="/truck" element={<TruckPage />} /> */}
-      {/* <Route path="/driver" element={<DriverPage />} /> */}
-      <Route path="/agent" element={<AgentPage />} />
-    </Routes>
+        </Routes>
   );
 }
 
